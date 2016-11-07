9063bae0605e9b3428b80b6040b0a62c7d3bf921
Aluno <Aluno@4SLP2V1>
2016-11-07 18:27:28 -0200

    Primeiro commit - código básico

---
+++ src/br/com/esmero/main/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.esmero.main;
+
+import br.com.esmero.view.index;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Main {
+    public final static ResourceBundle myResources = ResourceBundle.getBundle("messages", Locale.getDefault());
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(index.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new index().setVisible(true);
+        });
+    }
+    
+}
